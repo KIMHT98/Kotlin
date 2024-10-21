caf9a01c16b62eb9db53f6b732f1c2a99888f017
kht19684 <kht19684@gmail.com>
2024-10-21 18:20:45 +0900

    15장 진행 중

---
+++ Ch_15_Service/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ch_15_Service"
+include(":app")
+include(":ch15_outer")
